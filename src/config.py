72c415a6421043abf982477a7a1478e11a82b514
jackson-simionato <simionato.jackson@gmail.com>
2025-08-03 01:50:49 -0300

    update ApplicationConfig to include processed data path

--- src/config.py
+++ src/config.py
@@ -8,3 +8,5 @@ class ApplicationConfig:
         # Get the root project directory (parent of src folder)
         project_root = Path(__file__).parent.parent
         self.data_path = project_root / 'data' / 'raw' / 'Test O_G_Equipment_Data.xlsx - O&G Equipment Data.csv'
+        self.processed_data_path = Path(project_root / 'data' / 'processed' / 'df_fpso_failure_monitoring_processed.csv')
+        self.processed_data_path.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
