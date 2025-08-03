41a8919eec85b0c8b9f31d96dd78f503305cb6b9
jackson-simionato <simionato.jackson@gmail.com>
2025-08-03 10:43:49 -0300

    assert that data_path exists on ApplicationConfig initialization

--- src/config.py
+++ src/config.py
@@ -7,6 +7,9 @@ class ApplicationConfig:
     def __init__(self):
         # Get the root project directory (parent of src folder)
         project_root = Path(__file__).parent.parent
+        
         self.data_path = project_root / 'data' / 'raw' / 'Test O_G_Equipment_Data.xlsx - O&G Equipment Data.csv'
+        assert self.data_path.exists(), f"Data file not found at {self.data_path}"
+
         self.processed_data_path = Path(project_root / 'data' / 'processed' / 'df_fpso_failure_monitoring_processed.csv')
         self.processed_data_path.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
